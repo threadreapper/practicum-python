661f3239d8c2889b7194eefe1ee9731395097f91
timothy <caveiroussight@gmail.com>
2025-10-14 20:08:32 +0600

    fixed the output and inverted matrixes

--- 1.py
+++ 1.py
@@ -15,21 +15,32 @@ b = B.sum(axis=0)
 print(np.size(a),np.size(b))
 
 multiplication = A @ B
+print(multiplication)
 
 A = np.delete(A, 3, axis=1)
 new_cols = np.random.randint(10, 20, 18).reshape(6, 3)
 B = np.hstack((B, new_cols))
+print(A, B)
 
 det_A = np.linalg.det(A)
 det_B = np.linalg.det(B)
-if ((det_A != 0) and (det_B != 0)):
+print(det_A, det_B)
+
+if det_A != 0:
     inv_A = np.linalg.inv(A)
-    inv_B = np.linalg.inv(B)
+    print(inv_A)
 else:
-    print("невозможно найти обратные матрицы")   
-    
+    print("невозможно найти обратную матрицу для A")   
+
+if det_B != 0:
+    inv_B = np.linalg.inv(B)
+    print(inv_B)
+else:
+    print("невозможно найти обратную матрицу для B")
+
 A = np.linalg.matrix_power(A, 6)
 B = np.linalg.matrix_power(A, 14)
+print(A, B)
 
 # мой вариант - 2
 coefficients = np.array((2, -5, 1, 0, 1, -1, 13, 0, 3, -2, -2, -4, 4, 0, 2.7, -1.3)).reshape(4, 4)
